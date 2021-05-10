a4352762980ae4f90bdf1198a24cde47c2b26810
Emerson Costa <emersoncs@outlook.com.br>
2021-05-10 09:49:15 -0300

    login rede social obtendo imagem de perfil
    Agora o login com redes sociais, também trazem a imagem de perfil.

--- PI5/app/Http/Controllers/RedeSocialLoginController.php
+++ PI5/app/Http/Controllers/RedeSocialLoginController.php
@@ -33,6 +33,30 @@ class RedeSocialLoginController extends Controller
 
          );
 
+        // Tenta obter imagem de perfil
+        try
+        {
+            if ( empty($user->image) ) // Apenas grava imagem da rede social se já não houver imagem salva no perfil
+            {
+                $imagem = $userProvider->getAvatar();
+
+                $data               = file_get_contents($imagem);
+                $dataEncoded        = base64_encode($data);
+                $imagem_convertida  = "data:image/jpeg;base64,$dataEncoded";
+
+                if ( !empty($imagem_convertida) )
+                {
+                    $user->image = $imagem_convertida;
+                    $user->save();
+                }
+            }
+        }
+
+        catch (\Throwable $th)
+        {
+            session()->flash('error', "Desculpe, mas não possivel obter a sua imagem de perfil.");
+        }
+
          Auth::login( $user );
 
          return redirect( route( 'index' ) );
